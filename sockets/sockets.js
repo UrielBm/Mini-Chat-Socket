502a448b5574f3546dc6e2e91cb23395fdf2e88c
Uriel Benítez Medina <urielbenitez96@hotmail.com>
2021-06-10 11:08:59 -0500

    agrgando un campo de usuario

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -8,6 +8,7 @@ class Sockets {
       socket.on("mensaje-to-server", (mensaje) => {
         console.log(mensaje);
         this.io.emit("mensaje-from-server", {
+          user: mensaje.user,
           msg: mensaje.msg,
           date: new Date(),
         });
